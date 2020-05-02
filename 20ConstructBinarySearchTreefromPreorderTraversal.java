e4bf1a1be52f10203f7be7af0f233076017584d6
Dexterzprotege <42378552+Dexterzprotege@users.noreply.github.com>
2020-05-02 13:50:17 +0530

    Create 20ConstructBinarySearchTreefromPreorderTraversal.java

---
+++ 20ConstructBinarySearchTreefromPreorderTraversal.java
@@ -0,0 +1,58 @@
+/**Return the root node of a binary search tree that matches the given preorder traversal.
+
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+
+Example 1:
+
+        8
+       / \
+      5   10
+     / \.   \
+    1   7.   12
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+
+Constraints:
+1 <= preorder.length <= 100
+1 <= preorder[i] <= 10^8
+The values of preorder are distinct.**/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    static int p = 0;
+    static TreeNode build(int[] inorder, int[] preorder, int start, int end){
+        if(start>end)
+            return null;
+        TreeNode root = new TreeNode(preorder[p++]);
+        if(start == end)
+            return root;
+        int index = search(inorder, start, end, root.val);
+        root.left = build(inorder, preorder, start, index-1);
+        root.right = build(inorder, preorder, index+1, end);
+        return root;
+    }
+    static int search(int arr[], int strt, int end, int value) { 
+        int i; 
+        for (i = strt; i <= end; i++) { 
+            if (arr[i] == value) 
+                return i; 
+        } 
+        return i; 
+    } 
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder.length == 1)
+            return new TreeNode(preorder[0]);
+        int[] pre = preorder.clone();
+        Arrays.sort(preorder);
+        return build(preorder, pre, 0, preorder.length-1);
+    }
+}
